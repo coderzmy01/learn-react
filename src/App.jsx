d3332f11290208e482d4a991cbe4fb7fc6efbf63
coderzmy <principalming@outlook.com>
2025-04-15 23:50:25 +0800

    feat:重置模板文件

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,10 @@
 // src/App.jsx
 import React from 'react';
-import { Link, Outlet } from 'react-router-dom';
 
 const App = () => {
   return (
     <div>
-      {/* 导航栏 */}
-      <nav className="nav">
-        <ul className="nav-list">
-          <li>
-            <Link to="/">Tip Calculator</Link>
-          </li>
-          <li>
-            <Link to="/travel-list">Travel List</Link>
-          </li>
-        </ul>
-      </nav>
-
-      {/* 路由内容出口 */}
-      <Outlet />
+      <div>Hello World</div>
     </div>
   );
 };
