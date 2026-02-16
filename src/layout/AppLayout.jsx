143bcd521e6cfe8fed632930760229e2cf28023f
applezmy <principalming@outlook.com>
2026-02-16 14:37:02 +0800

    feat:add loading

--- src/layout/AppLayout.jsx
+++ src/layout/AppLayout.jsx
@@ -2,15 +2,22 @@ import React from "react";
 import Header from "./components/Header";
 import { Outlet } from "react-router-dom";
 import CartOverview from "../features/cart/CartOverview";
+import { useNavigation } from "react-router-dom";
+import { Spinner } from "../components/Spinner";
 
 const AppLayout = () => {
+  const { state } = useNavigation();
   return (
-    <div>
-      <Header />
-      <main>
-        <Outlet />
-      </main>
-      <CartOverview />
+    <div className="layout">
+      {state === "loading" && <Spinner />}(
+      <>
+        <Header />
+        <main>
+          <Outlet />
+        </main>
+        <CartOverview />
+      </>
+      )
     </div>
   );
 };
